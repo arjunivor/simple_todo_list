574037d744f80f2be24f3ae66a9f9dec6b29e9f2
arjunivor <arjunkukreja330@gmail.com>
2023-05-19 22:36:30 +0530

    mid session commit

--- main.js
+++ main.js
@@ -5,5 +5,20 @@ window.addEventListener("load", () => {
 
   form.addEventListener("submit", (e) => {
     e.preventDefault;
+
+    const task = input.value;
+
+    if (!task) {
+      alert("please fill out the task");
+      return;
+    }
+
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+
+    task_el.appendChild(task_content_el);
   });
 });
